113fb56c1e2b000d2210620605ca2eafe16658e8
Ludovica-DOrsa <ludovicadorsa@icloud.com>
2024-09-11 10:15:19 +0200

    Added govt in charge to layout

--- app.R
+++ app.R
@@ -2,11 +2,14 @@ library(readxl)
 library(tidyverse)
 library(plotly)
 library(shiny)
+library(bslib)
+library(htmltools)
 
 show_message <- function(){message(Sys.time(), " updating hover_reactive")}
 
 govt <- read_xlsx(path = "data.xlsx", sheet = "GOVERNI")
 debt <- read_xlsx(path = "data.xlsx", sheet = "DATI")
+
 
 debt <- debt %>%
   pivot_longer(
@@ -16,7 +19,7 @@ debt <- debt %>%
     values_drop_na = TRUE)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- page_fillable(
 
     # Application title
     titlePanel("Esplorazione del debito pubblico italiano"),
@@ -24,15 +27,9 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput(
-              "daterange",
-              "Scegli un range di anni:",
-              min = 1948,
-              max = 2022,
-              value = c(2012, 2022),
-              dragRange = TRUE,
-              sep = ""
-            ),
+            sliderInput("daterange", "Scegli un range di anni:", min = 1948,
+                        max = 2022, value = c(2012, 2022), dragRange = TRUE,
+                        sep = ""),
             selectInput("var", "Seleziona una statistica:", 
                         c("Popolazione a inizio anno",
                           "Pil reale",
@@ -51,16 +48,19 @@ ui <- fluidPage(
                           "Debito in valuta domestica",
                           "Debito in valuta estera",
                           "Deficit (-)/Surplus(+) operativo")),
-            selectInput("um", 
-                        "Seleziona un\'unità di misura:",
-                        unique(debt$`UNITA DI MISURA`))
-        ),
+            selectInput("um",  "Seleziona un\'unità di misura:",
+                        unique(debt$`UNITA DI MISURA`)),
+            card(height = 350, full_screen = TRUE, 
+                 card_header("Governo in carica:"),
+                 card_body(layout_column_wrap(width = 1/2, 
+                                              card_body(uiOutput("govt"),
+                                                        textOutput("dates"),
+                                                        textOutput("coal")),
+                                              card_body(imageOutput("pic")))))),
         
-
-        # Show a plot of the generated distribution
         mainPanel(
            plotlyOutput("year_var"),
-           tabPanel("hover data", verbatimTextOutput('text'))
+           
         )
     )
 )
@@ -70,8 +70,7 @@ server <- function(input, output, session) {
   
   observe({
     if(!is.null(input$var)){
-      updateSelectInput(session, "um", 
-                        choices = unique(debt[debt$ANNO == 
+      updateSelectInput(session, "um",  choices = unique(debt[debt$ANNO == 
                                                 input$var, ]$`UNITA DI MISURA`),
                         selected = unique(debt[debt$ANNO == 
                                                  input$var, ]$`UNITA DI MISURA`)[1])
@@ -84,7 +83,7 @@ server <- function(input, output, session) {
         filter(Year <= input$daterange[2]) %>%
         filter(`UNITA DI MISURA` == input$um) %>%
         filter(ANNO == input$var)
-      # draw the histogram with the specified number of bins
+      
       plot_ly(dat, x = ~Year, y = ~Value, type = "bar") %>%
         layout(xaxis = list(title = ''),
                yaxis = list(title = toString(input$um)))
@@ -94,13 +93,31 @@ server <- function(input, output, session) {
     observe({
       show_message()
       hover_data <- event_data("plotly_hover")
-      print(hover_data)
-      if (!is.null(hover_data))
-        hover_reactive(hover_data)                  ## set
-    })
+      if (!is.null(hover_data)){
+        hover_reactive(hover_data) 
+        
+        last_govt <- govt %>%
+          filter(format(govt$Inizio, format = "%Y") <= hover_data$x) %>% 
+          arrange(Inizio) %>%
+          slice(n())
+        
+        output$govt <- renderUI({
+          tagList(a(toString(last_govt$Governo), 
+                                 href = last_govt$Link))})
+        output$pic <- renderImage({
+          list(src = paste0(gsub(" ", "", toString(last_govt$Nome)), 
+                                     ".png"), width = 100)}, deleteFile = FALSE)
+        
+        output$dates <-  renderText({
+          paste0(last_govt$Inizio, " - ", last_govt$Fine)})
+        
+        output$coal <-  renderText({
+          last_govt$Coalizione})
+    }
     
-    output$text <- renderPrint({hover_reactive()})
     
+    
+})
     
 }
 
